aeea9e0b2bd6cc61014b9f2e3ca2550b53c25486
CNMan <nklijl@mail.nankai.edu.cn>
2021-12-22 20:32:06 +0000

    Wed 22 Dec 2021 08:32:06 PM GMT

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 wget -N -x -i dl.txt
 find ~/kuu/Updates/ -type f | sort | uniq >filelist
+rm -rf sha1sum
+cat filelist | while read line; do sha1sum $line >>sha1sum; done
 git add .
 git commit -m "`date`"
 git push -f origin master
