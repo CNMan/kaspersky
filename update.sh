30476671bd6dfb99fa5d6c9c0e31121ebb330448
CNMan <nklijl@mail.nankai.edu.cn>
2021-12-23 11:31:55 +0000

    Thu 23 Dec 2021 11:31:55 AM GMT

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
-wget -N -x -i dl.txt
 find ~/kuu/Updates/ -type f | sort | uniq >filelist
+grep '\.mft$' filelist >dl.txt
+grep '\.stt$' filelist >>dl.txt
+grep '\.set$' filelist >>dl.txt
+grep '\.xml$' filelist >>dl.txt
+sed -i 's/\/root\/kuu\/Updates/dnl-01.geo.kaspersky.com/g' dl.txt
+wget -N -x -i dl.txt
 rm -rf sha1sum
 cat filelist | while read line; do sha1sum $line >>sha1sum; done
 git add .
