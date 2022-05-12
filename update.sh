04cd9eab7f9b1692a6efc42c13869123c2840edc
CNMan <nklijl@mail.nankai.edu.cn>
2022-05-13 00:37:35 +0100

    Fri 13 May 2022 12:37:35 AM BST

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ sed -i 's/\/root\/kuu\/Updates/dnl-01.geo.kaspersky.com/g' dl.txt
 wget -N -x -i dl.txt
 rm -rf sha1sum
 cat filelist | while read line; do sha1sum $line >>sha1sum; done
+grep Filename dnl-01.geo.kaspersky.com/bases/as/kas5/0/kas5-0607g.xml | sed 's/"/\n/g' | sed 's/Filename=//g' | sort | uniq | grep '\-22' >diffkas
 git add .
 git commit -m "`date`"
 git push -f origin master
